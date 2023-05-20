5dc9dbff80410b2abaf03d87ab5cdf9a58d604f8
or-abdillh <hans.abdillh05@gmail.com>
2023-05-20 15:21:58 +0800

    Update migration

--- database/migrations/2023_05_19_034734_add_phone_number_to_users.php
+++ database/migrations/2023_05_19_034734_add_phone_number_to_users.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('phone_number');
+            $table->string('phone_number')->nullable()->default(null);
         });
     }
 
