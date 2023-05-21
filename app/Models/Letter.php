61e33d9b08c103107bc75607f598ccfb08214d2b
or-abdillh <hans.abdillh05@gmail.com>
2023-05-22 00:00:16 +0800

    Add soft delete at Letter model

--- app/Models/Letter.php
+++ app/Models/Letter.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Letter extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
     protected $guarded = ['id'];
+    protected $dates = ['deleted_at'];
 
     public function car()
     {
