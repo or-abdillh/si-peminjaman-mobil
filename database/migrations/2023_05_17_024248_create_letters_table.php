5fd7815201181ef7f765ed173c168d44f9577d6e
or-abdillh <hans.abdillh05@gmail.com>
2023-07-02 21:18:10 +0800

    Fix bug at user delete

--- database/migrations/2023_05_17_024248_create_letters_table.php
+++ database/migrations/2023_05_17_024248_create_letters_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->boolean('status')->nullable();
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('car_id')->references('id')->on('cars');
         });
 
